8dfc7c3899cfc571bc65b928fb69ca680c5b40d0
Nathan Laoue <nlaoue@nlaoue-lp.users.troie.ia>
2026-06-08 13:17:26 +0200

    feat: add local search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,6 +79,19 @@ const config: Config = {
     ],
   ],
 
+  themes: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        indexDocs: true,
+        indexBlog: true,
+        docsRouteBasePath: '/atlas',
+        language: ['en'],
+      },
+    ],
+  ],
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
